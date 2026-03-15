9d346a199e5f1af62f94efade01f5d5e6cbf6912
Henrique Amorim <henriqueamorim194@gmail.com>
2026-03-15 00:33:04 -0300

    botão de excluir funcional

--- app.js
+++ app.js
@@ -1,7 +1,12 @@
 let listaHistorico = document.getElementById("lista_historico");
 let listaMovimentos = [];
+let listaEntrada = [];
+let listaSaida = [];
 let subtituloHistorico = document.getElementById("subtitulo_historico");
 let historicoVazio = document.getElementById('historico_vazio');
+let somaEntrada = 0;
+let somaSaida = 0;
+
 
 function adicionar(){
 
@@ -25,25 +30,31 @@ let itensDaLista = {
 listaMovimentos.push(itensDaLista)
 atualizarTela()
 verificandoListaMovimentos()
+atualizaValor();
 }
+
 function atualizarTela(){
 listaHistorico.innerHTML = '';
 listaMovimentos.forEach((item, index) => {
     listaHistorico.innerHTML += 
 `<li class="item_historico">
 <div class="div_historico">
-<button class="historico_${item.tipo}">
-${item.tipo === "entrada" ? "Entrada" : "Saída"}
-</button>
+<button class="historico_${item.tipo}">${item.tipo === "entrada" ? "Entrada" : "Saída"}</button>
 <p class="historico_descricao">${item.descricao}</p>
 </div>
 <div class="div_historico">
 <p class="historico_valor">R$ ${item.valor.toFixed(2)}</p>
-<button class="botao_excluir" onclick="excluir(${index})">
-Excluir
-</button>
+<button class="botao_excluir" onclick="excluir(${index})">Excluir</button>
 </div>
 </li>`;})
+index++
+}
+
+
+function excluir(index){
+    listaMovimentos.splice(index, 1);
+    atualizarTela()
+    verificandoListaMovimentos()
 }
 
 function verificandoListaMovimentos(){
@@ -51,7 +62,35 @@ function verificandoListaMovimentos(){
         historicoVazio.style.display = 'none';
         listaHistorico.classList.add('lista_historico');
         listaHistorico.classList.remove('lista_historico_vazio');
-    }    
+    } else  {
+        historicoVazio.style.display = 'block';
+        listaHistorico.classList.add('lista_historico_vazio');
+        listaHistorico.classList.remove('lista_historico');
+    }  
 
-    console.log(listaHistorico.classList);
+    console.log(historicoVazio.style.display)
+}
+
+function atualizaValor(){
+
+
+    listaMovimentos.forEach((item) => {
+        if (item.tipo = 'entrada') {
+            listaEntrada.push(item.valor);
+                let i = 0;
+            somaEntrada += listaEntrada[i];
+
+        } else if (item.tipo = 'saida') {
+            listaSaida.push(item.valor)
+        }
+    })
+
+        for(i = 0; i < listaSaida.length; i++){
+        somaSaida = [];
+        somaSaida += listaSaida[i];
+    }
+
+    let saldo = document.getElementById('saldo_atual');
+
+    saldo.innerHTML = somaEntrada - somaSaida;
 }
